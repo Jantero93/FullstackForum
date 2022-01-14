55f885ec5dbab7132e17cc22ee1e54e9b04b62ad
Jantero93 <janzku1993@hotmail.com>
2022-01-14 20:19:09 +0200

    init frontend with eslint and prettier

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div id="App">
+      Edit <code>src/App.tsx</code> and save to reload.
+    </div>
+  );
+};
+
+export default App;
