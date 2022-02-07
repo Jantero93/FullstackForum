2cd5c3cebf8bdd8dc9891668c9064a3f0119e277
Jantero93 <janzku1993@hotmail.com>
2022-02-07 15:55:24 +0200

    init admin panel

--- frontend/src/components/pages/AdminPanel.tsx
+++ frontend/src/components/pages/AdminPanel.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const AdminPanel = (): JSX.Element => {
+/** UI */
+import { Tabs, Tab, Box } from '@mui/material';
+
+const AdminPanel: React.FC = (): JSX.Element => {
+  const [tabIndex, setTabIndex] = React.useState<number>(0);
+
   return (
-    <div>
-      <p>jotain</p>
-    </div>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs
+        centered
+        value={tabIndex}
+        onClick={(e: any) => setTabIndex(e.target.tabIndex)}
+        aria-label="basic tabs example"
+        variant={'fullWidth'}
+        textColor={'inherit'}
+        style={{color: 'whitesmoke'}}
+      >
+        <Tab label="Boards" tabIndex={0} />
+        <Tab label="Topics" tabIndex={1} />
+        <Tab label="Users" tabIndex={2} />
+      </Tabs>
+    </Box>
   );
 };
 
