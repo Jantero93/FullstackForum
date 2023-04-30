f918802d8bc063a6bc225ad1c3bbedc267f39913
Jani <janzku1993@hotmail.com>
2023-04-30 07:37:20 +0300

    init java back end

---
+++ fullstackforum-java/src/main/java/com/example/fullstackforum/test/TestController.java
@@ -0,0 +1,13 @@
+package com.example.fullstackforum.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String homeController() {
+        return "This is root/home route";
+    }
+}
---
+++ fullstackforum-java/src/test/java/com/example/fullstackforum/FullstackforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
